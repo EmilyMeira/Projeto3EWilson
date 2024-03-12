6cdfcd7896c580c6a2f22fca10f072e8e17277e7
Estudante Alura <105920438+EmilyMeira@users.noreply.github.com>
2024-03-12 08:48:23 -0300

    a

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const botoes=document.querySelectorAll(".botao");
 for( let i = 0 < botoes.length; j++){
   botoes [i].onclick = function(){
     for(let j=0; j< botoes.length; j++){
-        botes [j].classlist.remove("ativo");
+        botoes [j].classlist.remove("ativo");
     }
     botes[i].classlist.add ("ativo");
   }
